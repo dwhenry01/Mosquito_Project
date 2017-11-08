aff9adb1a62a09118a863dd6c3e7461f68cad320
dwhenry01 <32469840+dwhenry01@users.noreply.github.com>
2017-11-07 21:19:47 -0500

    Cleaned JS
    Deleted failed attempts at functions and added clearer comments for the remaining JS.

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ const xmos = document.querySelector('img.nomos');
 const sbmt = document.querySelector('button.button');
 
 
-
+/*Function for the Disagree Button*/
 
 disagreeClick.addEventListener('click', (event) => {
    alert("Wow, sorry you were so wrong. I'll give you another shot"); 
 });
 
+/*Function for the Submit button*/
 
 function error(){
     alert("Sorry, we are not set up to take newsletter registrations at this time. We apologize for any inconvience. Please enjoy the rest of the site.");
     submit.textContent = "Sorry!";
 }
 
+/*Function for the Immage Switch*/
 
 function imgswitch(){
     var mospic = document.getElementById('plainmos');
@@ -35,47 +37,3 @@ function imgswitch(){
 
 
 
-
-//Trying to get an alert to occur when the form submit button is pressed
-
-
-
-//sbmt.addEventListener('click', (event) => {
-//    alert("Sorry, we are not set up to take newsletter registrations at this time. We apologize for any inconvience. Please enjoy the rest of the site.");
-//});
-
-//sbmt.onclick = function (){
-//    alert("Sorry, we are not set up to take newsletter registrations at this time. We apologize for any inconvience. Please enjoy the rest of the site.");
-//};
-
-
-//Failed attempts at geting the pictures to switch when the agree button is pressed
-
-//agreeClick.addEventListener('click', (event) => {
-//   var mospic = document.getElementById('plainmos');
-//   var xmos = document.getElementById('nomos');
-//    if (mospic.parentNode){
-//        mospic.parentNode.removeChild(mospic);
-//        
-//                                  };
-//    
-//    
-//} );
-
-//$('#agree').one('click', function imgswitch(){
-//    var mospic = document.getElementById('plainmos');
-//    var xmos = document.getElementById('nomos');
-//    if (mospic.style.display === 'none'){
-//        mospic.style.display = 'block'; 
-//        xmos.style.display = 'none';
-//    } else {mospic.style.display = 'none'; 
-//            xmos.style.display = 'block';}
-//});
-
-
-//Failed attempt at getting the image switch button to disable after being clicked (this fucntion made the button disable after second click)
-
-
-//    document.getElementById("agree").onclick = function(){
-//        this.disabled = true;
-//    };
